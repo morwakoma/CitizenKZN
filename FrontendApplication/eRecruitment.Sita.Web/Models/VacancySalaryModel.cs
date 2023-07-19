d36dae83cfd5dc48d1d45869d388e5255796eb9b
khutso <khutso.mabelane@sita.co.za>
2023-07-19 19:00:54 +0200

    Citizen KZN Source code
    Citizen KZN Source code

---
+++ FrontendApplication/eRecruitment.Sita.Web/Models/VacancySalaryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eRecruitment.Sita.Web.Models
+{
+    public class VacancySalaryModel
+    {
+       public   int SalaryID { get; set; }
+       public  int JobTitleID { get; set; }
+       public   string JobLevel { get; set; }
+       public  decimal MinValue { get; set; }
+       public  decimal MaxValue { get; set; }
+       public  int VacancyID { get; set; }
+
+    }
+}
